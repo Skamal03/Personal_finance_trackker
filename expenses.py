cd45707541a0b2f98d9b5289640480dfb722d95d
Sunnan Kamal <79265528+Skamal03@users.noreply.github.com>
2024-06-22 10:56:06 +0100

    Update expenses.py

--- expenses.py
+++ expenses.py
@@ -1,6 +1,7 @@
 from tkinter import messagebox
 from abc import ABC, abstractmethod
 import mysql.connector
+import csv
 
 
 class Accounts(ABC):
@@ -9,48 +10,83 @@ class Accounts(ABC):
     def deposit_amount(self, amount):
         pass
 
+    def retrieve(self):
+        pass
+
     @abstractmethod
     def get_details(self):
         pass
 
 class UserAccount(Accounts):
 
-    def __init__(self, account_balance=000, account_number=5680456754, account_pin=0000):
+    def __init__(self, account_balance=0000, account_name="USER", account_number=5680456754, account_pin=4269):
+        self.acc_name = account_name
         self.acc_no = account_number
         self.acc_balance = account_balance
         self.__acc_pin = account_pin
 
-    def deposit_amount(self, amount):
+    def retrive(self):
+
+        with open("balance.csv", "r") as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            last_row = rows[-1]
+
+            self.acc_balance = int(last_row[3])
+
+    def withdraw(self, amount, date, pin):
+
+        if pin == user_account.__acc_pin:
+
+            if amount <= self.acc_balance:
+                self.acc_balance -= amount
+                messagebox.showinfo("SUCCESS", "Amount successfully withdrawn!")
+
+                with open("balance.csv", "a", newline="") as file:
+                    writer = csv.writer(file)
+                    writer.writerow([date, 0, amount, self.acc_balance])
+
+            else:
+                messagebox.showerror("Fail", "Not enough money to Withdraw")
+
+        else:
+            messagebox.showerror("ERROR", "Wrong PIN")
+
+    def deposit_amount(self, amount, date):
 
         if isinstance(amount, int):
             self.acc_balance += amount
             messagebox.showinfo("SUCCESS", "Amount successfully deposited!")
 
+            with open("balance.csv", "a", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow([date, amount, 0, self.acc_balance])
+
+
         else:
             messagebox.showinfo("FAIL", "enter correct values")
 
-    def set_pin(self, old_pin, new_pin):
-
-        if old_pin == self.__acc_pin:
-
-            self.__acc_pin = new_pin
-            messagebox.showinfo("SUCCESS", "pin changed successfully")
-
-        else:
-
-            messagebox.showinfo("WARNING", "you entered wrong pin!")
 
     def get_details(self):
-        messagebox.showinfo("DETAILS", f"Account Number: {self.acc_no}\nAccount Balance: {self.acc_balance}")
-
+        messagebox.showinfo("DETAILS", f"Account Name:{self.acc_name}\nAccount Number: {self.acc_no}"
+                                       f"\nAccount Balance: {self.acc_balance}")
 
 class Savings:
 
-    def __init__(self, user_account, savings=0):
+    def __init__(self, user_account, savings=00):
         self.user_account = user_account
         self.savings = savings
 
-    def deposit_amount(self, deposit):
+    def retrive(self):
+
+        with open("Savings_deposit.csv", "r") as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            last_row = rows[-1]
+
+            self.savings = int(last_row[2])
+
+    def deposit_amount(self, deposit, date):
 
         if isinstance(deposit,str):
             messagebox.showerror("ERROR","Invalid entry")
@@ -63,9 +99,13 @@ class Savings:
             self.savings += deposit
             messagebox.showinfo("SUCCESS","Amount successfully deposited in savings!")
 
+            with open("Savings_deposit.csv", "a", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow([date, deposit, self.savings])
+
     def get_details(self):
-        messagebox.showinfo("DETAILS",f"Account Number: {self.user_account.acc_no}\nAccount Balance: {self.user_account.acc_balance}"
-                f"\nSavings Balance: {self.savings}")
+        messagebox.showinfo("DETAILS", f"Account Name:{self.user_account.acc_name}\nAccount Number: {self.user_account.acc_no}"
+                                       f"\nAccount Balance: {self.user_account.acc_balance}\nSavings Balance{self.savings}")
 
 class Expense:
 
@@ -80,7 +120,7 @@ class Expense:
     def get_name(self,name):
         self.name = name
 
-    def get_category(self,category):
+    def get_category(self, category):
         self.category = category
 
     def get_date(self,date):
@@ -133,31 +173,17 @@ class Expense:
         )
         mycursor = mydb.cursor()
 
-        expense_id = id
-        query = "DELETE FROM expenses WHERE id=%s"
-        mycursor.execute(query, (expense_id,))
-        mydb.commit()
-        mydb.close()
+        try:
+            expense_id = id
+            query = "DELETE FROM expenses WHERE id=%s"
+            mycursor.execute(query, (expense_id,))
+            mydb.commit()
+            mydb.close()
 
-        print("Expense successfully deleted!")
+            print("Expense successfully deleted!")
 
-    def display_expenses(self):
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="6090",
-            database="expense"
-        )
-        mycursor = mydb.cursor()
-
-        query = "SELECT * FROM expenses"
-        mycursor.execute(query)
-        records = mycursor.fetchall()
-
-        for record in records:
-            print(record)
-
-        mydb.close()
+        except:
+            messagebox.showerror("ERROR", "invalid expense id")
 
 
 user_account = UserAccount()
